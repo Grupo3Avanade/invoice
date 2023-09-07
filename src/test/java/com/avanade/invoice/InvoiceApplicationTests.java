b893a8c5d2c9b7d62d92eec426acce7455ce44dc
Rodolfo Santana <rodolfo@fasters.com.br>
2023-09-07 17:39:00 -0300

    feat: initial commit

---
+++ src/test/java/com/avanade/invoice/InvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avanade.invoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
